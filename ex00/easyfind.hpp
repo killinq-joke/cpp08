874593f6cc3a5c3c9608372c316ae411025107d7
Zakaria Touzri <ztouzri@c2r9p2.42nice.fr>
2021-11-22 15:48:24 +0100

    ADDED: ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,16 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mout <mout@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 21:19:29 by mout              #+#    #+#             */
-/*   Updated: 2021/11/19 21:20:25 by mout             ###   ########.fr       */
+/*   Updated: 2021/11/22 15:44:31 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 # include <iostream>
+# include <vector>
+# include <list>
+# include <deque>
 
+template <typename T>
+int	easyfind(T arr, int occ);
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mout <mout@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 21:16:41 by mout              #+#    #+#             */
-/*   Updated: 2021/11/19 21:22:20 by mout             ###   ########.fr       */
+/*   Updated: 2021/11/22 15:47:31 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,34 @@
 template <typename T>
 int easyfind(T arr, int occ)
 {
-    
+	int	ret = 0;
+
+	for (auto i = arr.begin(); i != arr.end(); i++)
+	{
+		if (*i == occ)
+			return (ret);
+		ret++;
+	}
+    return (-1);
 }
 
 int main(void)
 {
+	int	arrsize = 10;
+	int a[] = { 1, 5, 2, 6, 77, 4, 3, 1 , 9, 11};
+	std::list<int> arr1;
+	std::vector<int> arr2;
+	std::deque<int> arr3;
+	int	occ = 0;
+
+	for (int i = 0; i < arrsize; i++)
+	{
+		arr1.push_back(a[i]);
+		arr2.push_back(a[i]);
+		arr3.push_back(a[i]);
+	}
+	std::cout << "first occurence of: " << occ << " is index: " << easyfind(arr1, occ) << std::endl;
+	std::cout << "first occurence of: " << occ << " is index: " << easyfind(arr2, occ) << std::endl;
+	std::cout << "first occurence of: " << occ << " is index: " << easyfind(arr3, occ) << std::endl;
     return (0);
 }
